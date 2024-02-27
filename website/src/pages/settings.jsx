0bf1e0372d3ec6f052fc75fb8b0c53cca4a818f4
IvanLorenzoFernandez <ivantablet98@gmail.com>
2024-02-27 01:08:30 +0100

    Expanded Settings
    Added Inventory and Post Tutorial Inventory settings to the Settings page and prettified it, included necessary PHP changes.

--- website/src/pages/settings.jsx
+++ website/src/pages/settings.jsx
@@ -1,8 +1,44 @@
 import settingsData from '../../../config.json';
+import inventorySettingsData from '../../../static/fixed_responses/inventory.json';
+import postTutorialInventorySettingsData from '../../../static/fixed_responses/postTutorialInventory.json';
 import { useState } from "react";
 import $ from "jquery";
 
 export default function Settings() {
+    const [changeSettings, setChangeSettings] = useState(false);
+
+    const handleChangeSetting = () => {
+        setChangeSettings((prevState) => !prevState);
+    };
+
+    if (!changeSettings) {
+        return (
+            <>
+                <h1>Settings</h1>
+                <div className='sendMeLeft'>
+                    <button onClick={handleChangeSetting}>
+                        Change Inventory Settings
+                    </button>
+                    <Config />
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <h1>Settings</h1>
+                <div className='sendMeLeft'>
+                    <button onClick={handleChangeSetting}>
+                        Change Config Settings
+                    </button>
+                    <Inventory />
+                </div>
+            </>
+        )
+    }
+}
+
+function Config() {
     const [result, setResult] = useState("");
 
 	const handleSubmit = (e) => {
@@ -20,30 +56,115 @@ export default function Settings() {
 
     return (
         <>
-            <h1>Settings</h1>
             <form action="http://localhost:8000/server.php" method="post" onSubmit={(event) => handleSubmit(event)} className="settings-form">
-                <Option optionName={"skipStoryModeChoice"} check={settingsData.skipStoryModeChoice} text={"Skip the choice between Awakening and Duviri?"} />
-                <Option optionName={"skipTutorial"} check={settingsData.skipTutorial} text={"Skip the tutorial?"} />
-                <Option optionName={"unlockAllMissions"} check={settingsData.unlockAllMissions} text={"Unlock all Star Chart nodes?"} />
-                <Option optionName={"unlockAllQuests"} check={settingsData.unlockAllQuests} text={"Unlock all Quests?"} />
-                <Option optionName={"infiniteResources"} check={settingsData.infiniteResources} text={"Do you want infinite credits and platinum?"} />
-                <Option optionName={"unlockallShipFeatures"} check={settingsData.unlockallShipFeatures} text={"Do you want to unlock all Ship Segments?"} />
-                <Option optionName={"unlockAllShipDecorations"} check={settingsData.unlockAllShipDecorations} text={"Do you want to unlock all Ship Decorations?"} />
+                <input type="hidden" name="idFile" id="idFile" value="config.json" />
+                <table>
+                    <tbody>
+                        <Option optionName={"skipStoryModeChoice"} check={settingsData.skipStoryModeChoice} text={"Skip the choice between Awakening and Duviri?"} />
+                        <Option optionName={"skipTutorial"} check={settingsData.skipTutorial} text={"Skip the tutorial?"} />
+                        <Option optionName={"unlockAllMissions"} check={settingsData.unlockAllMissions} text={"Unlock all Star Chart nodes?"} />
+                        <Option optionName={"unlockAllQuests"} check={settingsData.unlockAllQuests} text={"Unlock all Quests?"} />
+                        <Option optionName={"infiniteResources"} check={settingsData.infiniteResources} text={"Do you want infinite credits and platinum?"} />
+                        <Option optionName={"unlockallShipFeatures"} check={settingsData.unlockallShipFeatures} text={"Do you want to unlock all Ship Segments?"} />
+                        <Option optionName={"unlockAllShipDecorations"} check={settingsData.unlockAllShipDecorations} text={"Do you want to unlock all Ship Decorations?"} />
+                    </tbody>
+                </table>
                 <input type="submit" value="Submit" />
             </form>
             <h1>{result}</h1>
         </>
-    )
+    )          
+}
+
+function Inventory() {
+    const [result, setResult] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const form = $(e.target);
+		$.ajax({
+			type: "POST",
+			url: form.attr("action"),
+			data: form.serialize(),
+			success(data) {
+				setResult(data);
+			},
+		});
+	};
+
+    if (settingsData.skipStoryModeChoice) {
+        return (
+            <>
+                <h1>POST TUTORIAL INVENTORY</h1>
+                <form action="http://localhost:8000/server.php" method="post" onSubmit={(event) => handleSubmit(event)} className="settings-form">
+                    <input type="hidden" name="idFile" id="idFile" value="postTutorialInventory.json" />
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    Maximum Reputation Per Reset:
+                                </td>
+                                <td>
+                                    <input type='number' name='reputationCaps' id="reputationCaps" defaultValue={postTutorialInventorySettingsData.DailyAffiliation}></input>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Maximum Focus Per Reset:
+                                </td>
+                                <td>
+                                <input type='number' name='focusCaps' id='focusCaps' defaultValue={postTutorialInventorySettingsData.DailyFocus}></input>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <input type="submit" value="Submit" />
+                </form>
+                <h1>{result}</h1>
+            </>
+        )
+    }else{
+        return (
+            <>
+                <h1>INVENTORY</h1>
+                <form action="http://localhost:8000/server.php" method="post" onSubmit={(event) => handleSubmit(event)} className="settings-form">
+                    <input type="hidden" name="idFile" id="idFile" value="inventory.json" />
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    Maximum Reputation Per Reset:
+                                </td>
+                                <td>
+                                    <input type='number' name='reputationCaps' id="reputationCaps" defaultValue={postTutorialInventorySettingsData.DailyAffiliation}></input>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Maximum Focus Per Reset:
+                                </td>
+                                <td>
+                                <input type='number' name='focusCaps' id='focusCaps' defaultValue={postTutorialInventorySettingsData.DailyFocus}></input>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <input type="submit" value="Submit" />
+                </form>
+                <h1>{result}</h1>
+            </>
+        )
+    }
 }
 
 function Option(props) {
     return (
         <>
-            <label>
-            {props.text}
-            <input type="radio" id={props.optionName} name={props.optionName} defaultChecked={props.check} value="true"/> Yes
-            <input type="radio" id={props.optionName} name={props.optionName} defaultChecked={!props.check} value="false"/> No
-            </label><br/>
+            <tr>
+                <td>{props.text}</td>
+                <td><input type="radio" id={props.optionName} name={props.optionName} defaultChecked={props.check} value="true"/> Yes</td>
+                <td><input type="radio" id={props.optionName} name={props.optionName} defaultChecked={!props.check} value="false"/> No</td>
+            </tr>
         </>
     )
 }
--- website/src/php/server.php
+++ website/src/php/server.php
@@ -1,52 +1,143 @@
 <?php
     header('Access-Control-Allow-Origin: http://localhost:5173');
-    $settings = file_get_contents("../../../config.json");
-    $settingsData = json_decode($settings, true);
-    foreach ($settingsData as $key => $val) {
-        if ($key=='skipStoryModeChoice'){
-            if (isset($_POST['skipStoryModeChoice'])){
-                $settingsData[$key]=$_POST['skipStoryModeChoice']=="true" ? true : false;
-                $val=$_POST['skipStoryModeChoice']=="true" ? 'true' : 'false';
+    if ($_POST['idFile']=="config.json") {
+        $settings = file_get_contents("../../../config.json");
+        $settingsData = json_decode($settings, true);
+        foreach ($settingsData as $key => $val) {
+            if ($key=='skipStoryModeChoice'){
+                if (isset($_POST['skipStoryModeChoice'])){
+                    $settingsData[$key]=$_POST['skipStoryModeChoice']=="true" ? true : false;
+                }
+            }
+            if ($key=='skipTutorial'){
+                if (isset($_POST['skipTutorial'])){
+                    $settingsData[$key]=$_POST['skipTutorial']=="true" ? true : false;
+                }
+            }
+            if ($key=='unlockAllMissions'){
+                if (isset($_POST['unlockAllMissions'])){
+                    $settingsData[$key]=$_POST['unlockAllMissions']=="true" ? true : false;
+                }
+            }
+            if ($key=='unlockAllQuests'){
+                if (isset($_POST['unlockAllQuests'])){
+                    $settingsData[$key]=$_POST['unlockAllQuests']=="true" ? true : false;
+                }
+            }
+            if ($key=='infiniteResources'){
+                if (isset($_POST['infiniteResources'])){
+                    $settingsData[$key]=$_POST['infiniteResources']=="true" ? true : false;
+                }
+            }
+            if ($key=='unlockallShipFeatures'){
+                if (isset($_POST['unlockallShipFeatures'])){
+                    $settingsData[$key]=$_POST['unlockallShipFeatures']=="true" ? true : false;
+                }
+            }
+            if ($key=='unlockAllShipDecorations'){
+                if (isset($_POST['unlockAllShipDecorations'])){
+                    $settingsData[$key]=$_POST['unlockAllShipDecorations']=="true" ? true : false;
+                }
             }
         }
-        if ($key=='skipTutorial'){
-            if (isset($_POST['skipTutorial'])){
-                $settingsData[$key]=$_POST['skipTutorial']=="true" ? true : false;
-                $val=$_POST['skipTutorial']=="true" ? 'true' : 'false';
-            }
-        }
-        if ($key=='unlockAllMissions'){
-            if (isset($_POST['unlockAllMissions'])){
-                $settingsData[$key]=$_POST['unlockAllMissions']=="true" ? true : false;
-                $val=$_POST['unlockAllMissions']=="true" ? 'true' : 'false';
-            }
-        }
-        if ($key=='unlockAllQuests'){
-            if (isset($_POST['unlockAllQuests'])){
-                $settingsData[$key]=$_POST['unlockAllQuests']=="true" ? true : false;
-                $val=$_POST['unlockAllQuests']=="true" ? 'true' : 'false';
-            }
-        }
-        if ($key=='infiniteResources'){
-            if (isset($_POST['infiniteResources'])){
-                $settingsData[$key]=$_POST['infiniteResources']=="true" ? true : false;
-                $val=$_POST['infiniteResources']=="true" ? 'true' : 'false';
-            }
-        }
-        if ($key=='unlockallShipFeatures'){
-            if (isset($_POST['unlockallShipFeatures'])){
-                $settingsData[$key]=$_POST['unlockallShipFeatures']=="true" ? true : false;
-                $val=$_POST['unlockallShipFeatures']=="true" ? 'true' : 'false';
-            }
-        }
-        if ($key=='unlockAllShipDecorations'){
-            if (isset($_POST['unlockAllShipDecorations'])){
-                $settingsData[$key]=$_POST['unlockAllShipDecorations']=="true" ? true : false;
-                $val=$_POST['unlockAllShipDecorations']=="true" ? 'true' : 'false';
-            }
-        }
+        $updatedSettingsData = json_encode($settingsData, JSON_PRETTY_PRINT);
+        file_put_contents('../../../config.json', $updatedSettingsData);
+        echo ("Changes have been saved!");
     }
-    $updatedSettingsData = json_encode($settingsData, JSON_PRETTY_PRINT);
-    file_put_contents('../../../config.json', $updatedSettingsData);
-    echo ("Changes have been saved!");
+    if ($_POST['idFile']=="postTutorialInventory.json") {
+        $postTutorialInventory = file_get_contents('../../../static/fixed_responses/postTutorialInventory.json');
+        $postTutorialInventoryData = json_decode($postTutorialInventory, true);
+        foreach ($postTutorialInventoryData as $key => $val) {
+            if ($key=='DailyAffiliation'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationPvp'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationLibrary'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationCetus'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationQuills'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationSolaris'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationVentkids'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationVox'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationEntrati'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationNecraloid'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationZariman'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationKahl'){
+                $postTutorialInventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyFocus'){
+                $postTutorialInventoryData[$key]=$_POST['focusCaps'];
+            }
+        }
+        $updatedPostTutorialInventoryData = json_encode($postTutorialInventoryData, JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK);
+        file_put_contents('../../../static/fixed_responses/postTutorialInventory.json', $updatedPostTutorialInventoryData);
+        echo ("Changes have been saved!");
+    }
+    if ($_POST['idFile']=="inventory.json") {
+        $inventory = file_get_contents('../../../static/fixed_responses/inventory.json');
+        $inventoryData = json_decode($inventory, true);
+        foreach ($inventoryData as $key => $val) {
+            if ($key=='DailyAffiliation'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationPvp'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationLibrary'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationCetus'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationQuills'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationSolaris'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationVentkids'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationVox'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationEntrati'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationNecraloid'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationZariman'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyAffiliationKahl'){
+                $inventoryData[$key]=$_POST['reputationCaps'];
+            }
+            if ($key=='DailyFocus'){
+                $inventoryData[$key]=$_POST['focusCaps'];
+            }
+        }
+        $updatedInventoryData = json_encode($inventoryData, JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK);
+        file_put_contents('../../../static/fixed_responses/inventory.json', $updatedInventoryData);
+        echo ("Changes have been saved!");
+    }
 ?>
